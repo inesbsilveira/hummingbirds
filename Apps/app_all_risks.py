5d4400e9c6649d4a4d2d5ba404b013b1851b7af4
Inês Silveira <isilveira@edu.ulisboa.pt>
2025-04-02 15:31:38 +0200

    Update app_all_risks.py

--- Apps/app_all_risks.py
+++ Apps/app_all_risks.py
@@ -16,8 +16,7 @@ import seaborn as sns
 from google.oauth2 import service_account
 from ee import oauth
 import statistics
-from streamlit_folium import folium_static
-import geemap.folium as geemap
+
 
 # Access credentials from Streamlit secrets
 secrets = st.secrets["GOOGLE_CREDENTIALS"]
