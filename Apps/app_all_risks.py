2dc0da46602e7b02f8f05b6b14da05acd643e3b8
Inês Silveira <inesinghambs@gmail.com>
2025-04-09 16:00:01 +0200

    Update app_all_risks.py

--- Apps/app_all_risks.py
+++ Apps/app_all_risks.py
@@ -807,7 +807,7 @@ if uploaded_shp:
             if st.button("Process"):
                 # Process the files
                 region, elevation, vis_params, elevation_mean_value, elevation_min_value, elevation_max_value, slope_mode, slope_mean, slope_min, slope_max, df_elevation, risk_level_erosion, avg_temp, min_temp_value, max_temp_value, hot_days_count_24, average, risk_level_thermal, total_precipitation, wet_precip_value, dry_precip_value, mean_precipitation, precipitation, years, total_floods, risk_level_f, percentage_drought, chart_list, chart_data, risk_level, mean_area_percentage, big_fire_frequency, risk_level_wf, df_wf = process_files(
-                    shp_file, start_date, end_date, dry_season_1stmonth, dry_season_lastmonth, wet_season_1stmonth, wet_season_lastmonth, wf_startDate, wf_endDate
+                    shp_file, start_date, end_date, dry_season_1stmonth, dry_season_lastmonth, wet_season_1stmonth, wet_season_lastmonth
                 )
 
                 # Display results
