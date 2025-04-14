0686d73336e75ac7ebf5812571916cb973961bfd
Inês Silveira <inesinghambs@gmail.com>
2025-04-14 10:52:57 +0200

    Update app_all_risks.py

--- Apps/app_all_risks.py
+++ Apps/app_all_risks.py
@@ -291,16 +291,16 @@ def process_files(shp_file, start_date, end_date, dry_season_1stmonth, dry_seaso
         })
 
         class color:
-       PURPLE = '\033[95m'
-       CYAN = '\033[96m'
-       DARKCYAN = '\033[36m'
-       BLUE = '\033[94m'
-       GREEN = '\033[92m'
-       YELLOW = '\033[93m'
-       RED = '\033[91m'
-       BOLD = '\033[1m'
-       UNDERLINE = '\033[4m'
-       END = '\033[0m'
+           PURPLE = '\033[95m'
+           CYAN = '\033[96m'
+           DARKCYAN = '\033[36m'
+           BLUE = '\033[94m'
+           GREEN = '\033[92m'
+           YELLOW = '\033[93m'
+           RED = '\033[91m'
+           BOLD = '\033[1m'
+           UNDERLINE = '\033[4m'
+           END = '\033[0m'
 
     #load the datasets
     #temperature - ERA5-Land dataset (temperature in Kelvin)
